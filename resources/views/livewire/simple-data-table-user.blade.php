769d3e4305ee6b6c67ad679ad3bf6db6f44f3498
Steve Creekmore <Creekmore108@gmail.com>
2022-11-30 23:49:25 -0700

    initial commit

---
+++ resources/views/livewire/simple-data-table-user.blade.php
@@ -0,0 +1,6 @@
+<div class="sm:align-center sm:flex sm:flex-col">
+        <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-center">Simple Datatable Users</h1>
+</div>
+<div>
+<livewire:datatable model="App\Models\User" name="users" />
+</div>
